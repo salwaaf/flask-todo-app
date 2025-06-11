7bcdb759d1bf75a4ab404ba73b64607201d480e1
devops4sure <salwa.afir98@gmail.com>
2025-06-11 15:47:37 +0100

    expose metrics for Prometheus

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from prometheus_client import start_http_server, Counter
 
-main = Blueprint('main', __name__)
+REQUEST_COUNT = Counter('todo_requests_total', 'Total requests to the index route')
+
+start_http_server(8000)  # metrics available at localhost:8000
 
 @main.route('/')
 def index():
+    REQUEST_COUNT.inc()
     return render_template('index.html')
